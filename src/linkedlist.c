fa25d178ce7f4ce5cbfb9fbaf2940a3ca941435a
Ritta Mariya Mathew <rittamariyamathew2029@mca.ajce.in>
2025-09-29 00:37:43 +0530

    Update linkedlist.c
    Implementation of a singly linked list.

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+struct Node{
+int data;
+struct Node*link;
+};
+
+struct Node*createNode(int rno){
+struct Node*one=(struct Node*)malloc(sizeof(struct Node));
+one->data=rno;
+one->link=NULL;
+return one;
+}
